517707b0c8d06892f1fea7c531a9ab94e7f270b0
Matt <stygianvision@gmail.com>
2018-02-03 18:40:44 -0800

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-orml',
+  packages = ['orml'],
+  version = '0.1',
+  description = 'Django Object Relational Mapping Language',
+  author = 'Matt Roberts',
+  author_email = 'contact@maleero.com',
+  url = 'https://github.com/malero/django-orml',
+  download_url = 'https://github.com/malero/django-orml/archive/0.1.tar.gz',
+  keywords = ['django', 'orm', 'ply'],
+  classifiers = [],
+)
